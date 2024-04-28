30bba166cb5c0c7d581052623e9251e68034007a
solaymane19 <65737517+solaymane19@users.noreply.github.com>
2024-04-28 23:23:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+document.querySelector("#A").addEventListener("click",function(){
+    alert("This website is under development !!")
+})
+document.querySelector("h1").addEventListener("click",function(){
+   
+
+})
